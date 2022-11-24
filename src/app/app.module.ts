006e47b9302062f5cf36eccde3cb44e8df18bdf3
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-24 19:09:29 +0530

    Book Delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { UserLoginComponent } from './user-login/user-login.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookEntryComponent } from './book-entry/book-entry.component';
 import { BookSearchComponent } from './book-search/book-search.component';
+import { BookDeleteComponent } from './book-delete/book-delete.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { BookSearchComponent } from './book-search/book-search.component';
     UserLoginComponent,
     BookListComponent,
     BookEntryComponent,
-    BookSearchComponent
+    BookSearchComponent,
+    BookDeleteComponent
   ],
   imports: [
     BrowserModule,
